81ca7e7b70f419a6c0f403e139e860ebebcef06b
rsibishan <rsibishan@gmail.com>
2021-03-18 10:01:07 +0530

    🚚 Moved Furniture Codebase into src

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 import Tesla from './Tesla/src/screens/index';
 import OnBoarding from './OnBoarding/src/screens/index';
 import Nike from './Nike/src/screens/index';
-import Furniture from './Furniture/screens/index';
+import Furniture from './Furniture/src/screens/index';
 
 export class App extends Component {
     render() {
